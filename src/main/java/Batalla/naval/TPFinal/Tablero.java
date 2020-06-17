8204cd2b140aa5917d7dff9e56ea0cb9ac273dd7
JP2620 <saucedojuanpablo@gmail.com>
2020-06-17 02:34:17 -0300

    Elimino lineas al pedo y creo constante global

--- src/main/java/Batalla/naval/TPFinal/Tablero.java
+++ src/main/java/Batalla/naval/TPFinal/Tablero.java
@@ -2,6 +2,7 @@ package main.java.Batalla.naval.TPFinal;
 import java.util.ArrayList;
 
 public class Tablero implements Observer {
+	public static final int CANT_BARCOS = 6;
 	DisplayDelTablero display;
 	public Tablero(DisplayDelTablero display) {
 		this.display = display;
@@ -15,19 +16,13 @@ public class Tablero implements Observer {
     public void colocarBarcos() {
     	BarcosFactory factory = new BarcosFactory();
 		ArrayList<Barco> barcos = new ArrayList<Barco>();
-    	Barco corbeta1 = factory.createBarco("corbeta");
-		Barco corbeta2 = factory.createBarco("corbeta");
-		Barco corbeta3 = factory.createBarco("corbeta");
-		Barco fragata1 = factory.createBarco("fragata");
-		Barco fragata2 = factory.createBarco("fragata");
-		Barco destructor1 = factory.createBarco("destructor");
 		
-		barcos.add(corbeta1);
-		barcos.add(corbeta2);
-		barcos.add(corbeta3);
-		barcos.add(fragata1);
-		barcos.add(fragata2);
-		barcos.add(destructor1);
+		barcos.add(factory.createBarco("corbeta"));
+		barcos.add(factory.createBarco("corbeta"));
+		barcos.add(factory.createBarco("corbeta"));
+		barcos.add(factory.createBarco("fragata"));
+		barcos.add(factory.createBarco("fragata"));
+		barcos.add(factory.createBarco("destructor"));
 		display.colocarBarcos(barcos, 0); //esto esta harcodeado, como se me borro lo soluciono rapido asi
     }
 	public void checkDisparo(int a, int b) {
